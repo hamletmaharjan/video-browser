35140d8d594c7ba06ce0f49cd0ca01d8009d1cb8
hamlet <imhamlet.hams@gmail.com>
2020-03-21 23:30:33 +0545

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+    <SearchBar></SearchBar>
+</div>
+
+</template>
+
+
+<script>
+import SearchBar from './components/SearchBar';
+
+export default {
+    name: 'App',
+    components: {
+        SearchBar
+    }
+}
+</script>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <input v-on:input="onInput"/>
+    </div>
+</template>
+
+
+
+<script>
+export default {
+    name: 'SearchBar',
+    methods: {
+        onInput: function(event){
+            console.log(event.target.value);
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
