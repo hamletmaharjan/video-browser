a8f2a08bade45ab6d30ad55d86090b96e5e812ea
hamlet <imhamlet.hams@gmail.com>
2020-03-22 17:22:16 +0545

    added youtube api for video search

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,43 @@
 <template>
 <div>
-    <SearchBar></SearchBar>
+    <SearchBar v-on:termChange="onTermChange"></SearchBar>
+    <VideoList></VideoList>
+    {{ videos.length }}
 </div>
 
 </template>
 
 
 <script>
+import axios from 'axios';
 import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
+const API_KEY = 'AIzaSyDeZ438ELFQmChoVujUDb7usm0FU28sp14';
 
 export default {
     name: 'App',
     components: {
-        SearchBar
+        SearchBar,
+        VideoList
+    },
+    data: function(){
+        return {
+            videos:[]
+        };
+    },
+    methods: {
+        onTermChange: function(searchTerm){
+            axios.get('https://www.googleapis.com/youtube/v3/search',{
+                params:{
+                    key: API_KEY,
+                    type: 'video',
+                    part: 'snippet',
+                    q: searchTerm
+                }
+            }).then(response => {
+                this.videos = response.data.items
+            });
+        }
     }
 }
 </script>
