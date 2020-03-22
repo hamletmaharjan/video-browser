38a9691cb8f3e6f022fc3094b9d82b20caffd364
hamlet <imhamlet.hams@gmail.com>
2020-03-22 22:12:51 +0545

    ui improved for video list

--- src/components/VideoListItem.vue
+++ src/components/VideoListItem.vue
@@ -1,6 +1,10 @@
 <template>
-    <li class="list-group-item">
-        {{video.snippet.title}}
+    <li class="list-group-item media">
+        <img class="mr-3" v-bind:src="thumbnailUrl" />
+        <div class="media-body">
+            {{video.snippet.title}}
+        </div>
+        
     </li>
 </template>
 
@@ -8,6 +12,24 @@
 <script>
 export default {
     name: 'VideoListItem',
-    props: ['video']
+    props: ['video'],
+    computed: {
+        thumbnailUrl() {
+            return this.video.snippet.thumbnails.default.url;
+        }
+    }
 }
 </script>
+
+
+
+<style scoped>
+li {
+    display:flex;
+    cursor:pointer;
+}
+
+li:hover {
+    background-color: #eee;
+}
+</style>
